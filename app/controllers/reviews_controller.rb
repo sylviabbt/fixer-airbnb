2d870bc80f37a8cb2b13b267733027ba745d440a
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-04-02 18:04:15 +0800

    reviewscontroller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking = Booking.find(params[:booking_id])
+    if @review.save
+      redirect_to booking_path(@review.booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :booking_id)
+  end
+end
