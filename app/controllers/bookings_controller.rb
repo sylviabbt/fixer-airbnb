032ea5aaff0df2f3929571829246ca6d231f7bc7
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-04-02 17:34:10 +0800

    bookingController

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    respond_to do |format|
+      if @booking.save
+        format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+        format.json { render :show, status: :created, location: @booking }
+      else
+        format.html { render :new }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :total_price, :address, :contact_info, :user_id, :skill_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-      resources :skills do
-        resources :bookings, only: [:index, :new, :create] do
-      end
-      resources :bookings, only: [:show] do
-          resources :reviews, only: [:create, :new]
-        end
+  resources :skills do
+    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:show] do
+      resources :reviews, only: [:create, :new]
     end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bookings
   resources :skills
