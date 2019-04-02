03989aaf67b2d4056ff9ffa5b4fd5f0019a22ec9
Jo Gong <gongjun18@gmail.com>
2019-04-02 15:28:39 +0800

    working progress on controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,19 @@
 class SkillsController < ApplicationController
+
+  def index
+    @skills = Skill.all
+  end
+
+  def new
+    @skills = Skill.new
+  end
+
+  def create
+    @skills = Skill.new(skill_params)
+    if @skill.save
+      redirect_to
+  end
+
+
+
 end
