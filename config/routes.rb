fcb48df3b104626646adbf452d9a8d8b94b9bb54
Jo Gong <gongjun18@gmail.com>
2019-04-02 14:32:00 +0800

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+      resources :skills do
+        resources :bookings, only: [:index, :new, :create] do
+      end
+      resources :bookings, only: [:show] do
+          resources :reviews, only: [:create, :new]
+        end
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# /skills/3/bookings/5/reviews
