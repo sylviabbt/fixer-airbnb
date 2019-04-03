4ea05336658c1211f00977b218a2407a03c458df
Jo Gong <gongjun18@gmail.com>
2019-04-03 09:58:40 +0800

    solve conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
       resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bookings
-  resources :skills
-  resources :reviews
 end
 
 # /skills/3/bookings/5/reviews
